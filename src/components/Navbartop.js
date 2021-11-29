1947bb9eb0fd83acf3ba8b7551111f55942993e4
Pema Dorji Sherpa <pemarekdendorjee@gmail.com>
2021-11-29 19:24:24 +0600

    Created Navbartop
    Navbar top component

---
+++ src/components/Navbartop.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbartop from './components/Navbartop';
+function App() {
+  return (
+    <div className="container-nav">
+      <Navbartop/>
+    </div>
+  );
+}
+
+export default App;
